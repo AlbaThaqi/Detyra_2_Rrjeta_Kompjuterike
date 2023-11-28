48412a9024995d733ce36208039ab1262e29816f
Alba Thaqi <112955653+AlbaThaqi@users.noreply.github.com>
2023-11-28 22:59:05 +0100

    Added changes to the client code

--- client.cpp
+++ client.cpp
@@ -3,7 +3,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
-#pragma warning(disable:4996) 
+#pragma warning(disable:4996)
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
@@ -13,12 +13,14 @@ const char* IP_ADDRESS = "127.0.0.1";
 
 int main() {
     WSADATA wsaData;
+
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         cerr << "Failed to initialize Winsock." << endl;
         return -1;
     }
 
     int clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
     if (clientSocket == -1) {
         cerr << "Error creating client socket." << endl;
         WSACleanup();
@@ -30,49 +32,42 @@ int main() {
     serverAddress.sin_addr.s_addr = inet_addr(IP_ADDRESS);
     serverAddress.sin_port = htons(PORT);
 
-   while (true){
-      cout << "Enter credentials (username:password): ";
+    char buffer[1024];
+
+    while (true) {
+        cout << "Enter credentials (username:password): ";
         cin.getline(buffer, sizeof(buffer));
-     sendto(clientSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-     memset(buffer, 0, sizeof(buffer));
+
+        sendto(clientSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+        memset(buffer, 0, sizeof(buffer));
         int bytesRead = recvfrom(clientSocket, buffer, sizeof(buffer), 0, NULL, NULL);
-         cout << "Server response: " << buffer << endl;
+
+        cout << "Server response: " << buffer << endl;
+
+        if (strcmp(buffer, "Authentication successful. Access granted.") == 0) {
+
+            while (true) {
+                cout << "Enter a command ('read <file_name>', 'write <string> <file_name>, execute <command> or type 'exit' to end): ";
+                cin.getline(buffer, sizeof(buffer));
+
+                if (strcmp(buffer, "exit") == 0) {
+                    break;
+                }
+
+                sendto(clientSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+                memset(buffer, 0, sizeof(buffer));
+                int bytesRead = recvfrom(clientSocket, buffer, sizeof(buffer), 0, NULL, NULL);
+
+                cout << "Server Response: " << buffer << endl;
+            }
+        }
+
+    }
+
     closesocket(clientSocket);
     WSACleanup();
 
     return 0;
 }
-   if (strcmp(buffer, "Authentication successful. Access granted.") == 0) {
-
-       while (true) {
-
-           cout << "Enter a command ('read <file_name>', 'write <string> <file_name>, execute <command> or type 'exit' to end): ";
-
-           cin.getline(buffer, sizeof(buffer));
-
-           if (strcmp(buffer, "exit") == 0) {
-
-               break;
-
-           }
-sendto(clientSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-
- 
-
-                memset(buffer, 0, sizeof(buffer));
-
-                int bytesRead = recvfrom(clientSocket, buffer, sizeof(buffer), 0, NULL, NULL);
-
- 
-
-                cout << "Server Response: " << buffer << endl;
-
-            }
-
-        }
-
- 
-
-    }
-
-
