c1071578b8d058c95fd9257668cf6f30fb01ac80
Alba Thaqi <112955653+AlbaThaqi@users.noreply.github.com>
2023-11-30 22:19:23 +0100

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -12,23 +12,7 @@ const int PORT = 12345;
 const char* IP_ADDRESS = "127.0.0.1";
 
 int main() {
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        cerr << "Failed to initialize Winsock." << endl;
-        return -1;
-    }
-
-    int serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (serverSocket == -1) {
-        cerr << "Error creating server socket." << endl;
-        WSACleanup();
-        return -1;
-    }
-
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr(IP_ADDRESS);
-    serverAddress.sin_port = htons(PORT);
+ 
 
     if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
         cerr << "Error binding server socket to address and port." << endl;
